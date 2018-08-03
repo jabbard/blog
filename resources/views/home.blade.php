fdafd6d4db38b4ef012b9b15bb07db0fc2670573
jabbard <Potter123>
2018-08-03 08:59:05 +0545

    third class first portion

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,10 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>This is home page.</h1>
-    <a href="home">Home</a>
-    <a href="about">About</a>
-    <a href="contact">Contact</a>
-    <hr>
-</body>
-</html>
+@extends('layouts.master')
+
+@section('container')
+<h1>This is home page.</h1>
+<a href="home">Home</a>
+<a href="about">About</a>
+<a href="contact">Contact</a>
+<hr>
+
+@stop
